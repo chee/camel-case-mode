0d075fe09ed21d106eae4c22c0462118e73abf66
chee <chee@snake.dog>
2017-08-11 16:48:22 +0100

    begin camelCaseMode

---
+++ spec/camel-case-mode-view-spec.js
@@ -0,0 +1,9 @@
+'use babel'
+
+import CamelCaseModeView from '../lib/camel-case-mode-view'
+
+describe('CamelCaseModeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy')
+  })
+})
